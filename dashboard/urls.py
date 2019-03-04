65d56b746f0606039c0d178b7cdbeb32e44dd644
FiNkacho <sabaxuxunashvili@gmail.com>
2019-03-04 19:48:29 +0400

    Logout function

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('logout', views.logout_view, name='logout')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 def index(request):
     return render(request, 'dashboard/landing.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
