a4158a42f03e29f12f3875bda0dc5f6d28df09ff
FiNkacho <sabaxuxunashvili@gmail.com>
2019-03-04 20:15:34 +0400

    debug

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 def index(request):
-    return render(request, 'dashboard/landing.html')
-
+    if not request.user.is_authenticated:
+        return render(request, 'dashboard/landing.html')
+    return render(request, 'dashboard/dashboard.html')
 
 def logout_view(request):
     logout(request)
