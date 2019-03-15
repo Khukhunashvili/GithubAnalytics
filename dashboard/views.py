1aea74a01d2a5ab51a23a526a424e7b1d0d481f2
FiNkacho <sabaxuxunashvili@gmail.com>
2019-03-15 23:13:57 +0400

    added sample loop in template

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 def index(request):
+    data = {
+        "Followers": [{ "username": "buckyroberts" }, {"username": "llSourcell"}],
+        "Following": [{ "username": "buckyroberts" }, {"username": "llSourcell"}]
+    }
+
+
     if not request.user.is_authenticated:
         return render(request, 'dashboard/landing.html')
-    return render(request, 'dashboard/dashboard.html')
+    return render(request, 'dashboard/dashboard.html', { "data" : data })
 
 def logout_view(request):
     logout(request)
